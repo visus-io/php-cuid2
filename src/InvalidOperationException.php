14fbf3e1d31feb70d2c45b0466b00dcd36acae7f
Alan Brault <alan.brault@visus.io>
2025-06-22 10:36:24 -0400

    refactor(cuid2): leverage markrogoyski/math-php when GMP extension is unavailable
    Signed-off-by: Alan Brault <alan.brault@visus.io>

---
+++ src/InvalidOperationException.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Visus\Cuid2;
+
+use Exception;
+
+/**
+ * Exception that is thrown when a method call is invalid for the object's current state.
+ */
+final class InvalidOperationException extends Exception
+{
+}
